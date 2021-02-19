f7bfdea3c7d6d6461cb867d05f14452f66b558d6
George Lee <glee@chowly.com>
2021-02-19 10:45:37 -0600

    saving

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -2,6 +2,7 @@ class Flag
   FRAME_OFFSET = 5
   FULL_HEIGHT = 150
   FULL_WIDTH = 200
+  BASE_COLOR = '#A463F2'
 
   def create_flag
     frame = create_frame
@@ -19,7 +20,7 @@ class Flag
 
   def create_frame
     frame = Victor::SVG.new
-    frame.rect x: 0, y: 0, width: 300, height: 200, fill: '#A463F2'
+    frame.rect x: 0, y: 0, width: 300, height: 200, fill: BASE_COLOR
     frame.rect x: FRAME_OFFSET, y: FRAME_OFFSET, width: 200 - (FRAME_OFFSET*2), height: 150 - (FRAME_OFFSET*2), fill: 'white'
     return frame
   end
@@ -98,6 +99,13 @@ class Flag
     return flag_image
   end
 end
+
+# idea for future: build a color pallette from a random seed by calling https://www.thecolorapi.com/id?hex=24B1E0
+# https://www.thecolorapi.com/docs
+
+# https://icons8.com/icons/dotty 
+# https://icons8.com/icon/pack/cultures/dotty
+# can also use pallete to change path/fill of svg images
 
 
 # def processLogs(logs, maxSpan)
